f156c24895d912ab8ab6e6b09909bbd1e01a6055
Andrew Pletnev <andrewpletnev@MacBook-Pro-6.local>
2025-08-06 15:36:57 +0100

    sitemaps added using next.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://frequencyfortress.com',
-  generateRobotsTxt: false,
-  generateIndexSitemap: false
-}
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: [
+    '/private/*',
+    '/hidden',
+    '/orgasm404*' 
+  ],
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+    additionalSitemaps: ['https://frequencyfortress.com/sitemap.xml']
+  }
+};
